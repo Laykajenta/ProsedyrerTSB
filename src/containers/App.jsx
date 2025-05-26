0cc3baa6a50c6ef3e94b505f64863ca27cbf06a5
Laykajenta <marianne.odegaard@outlook.com>
2025-05-26 09:51:10 +0200

    endret på app.jsx koden

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,46 +1,38 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList.jsx";
 import SearchBox from "../components/SearchBox.jsx";
 import Scroll from "../components/Scroll.jsx";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: "",
-    };
-  }
-  componentDidMount() {
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
+
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
+      .then(response => response.json())
+      .then(users => { setRobots(users) });
+  },[])
+
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value)
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
-
-    console.log(filteredRobots);
-  };
-  render() {
-    const filteredRobots = this.state.robots.filter((robot) => {
-        return robot.name
-        .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
-    });
-    if (this.state.robots.length === 0) {
-      return <h1>Laster</h1>;
-    } else {
-      return (
-        <div className="tc">
-          <h1>Prosedyrer Finnmarkskollektivet</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      );
-    }
-  }
+  const filteredRobots = robots.filter(robot=> {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
+  //console.log(robots, searchfield);
+  return !robots.length? 
+    <h1>Laster</h1> :
+    (
+    <div className="tc">
+        <h1 className="overskrift">Prosedyrer Finnmarkskollektivet</h1>
+        
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <CardList robots={filteredRobots} />
+      </Scroll>
+    </div>
+  );
 }
+
 export default App;
